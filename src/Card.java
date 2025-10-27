fbff148864b9d1a3799357f2df825c86400258e2
Aryan Singh <169827318+CryptidFiles@users.noreply.github.com>
2025-10-27 17:44:00 -0400

    Updated card class comments

--- src/Card.java
+++ src/Card.java
@@ -2,7 +2,7 @@
 abstract public class Card {
     protected boolean isLightSideActive;
 
-    // Attributes shared by ALL cards
+    // Attributes shared by ALL cards. Each card object has both a color and a type for each side
     protected Color lightColor;
     protected Color darkColor;
 
@@ -12,10 +12,11 @@ abstract public class Card {
     // Blueprint for acting upon the play of the card
     public abstract boolean action(UNO_Game model, Player player);
 
-    // Determine if card can be played on the top of the stack
+    // Determine if card can be played on the top of the card in play pile stack
     public abstract boolean playableOnTop(Card otherCard);
 
 
+    //CardType return the correct type depending on which side is active.
     public CardType getType() {
         if (isLightSideActive) {
             return lightType;
@@ -24,6 +25,7 @@ abstract public class Card {
         }
     }
 
+    // getColor return the correct color depending on which side is active.
     public Color getColor() {
         if (isLightSideActive) {
             return lightColor;
@@ -32,14 +34,17 @@ abstract public class Card {
         }
     }
 
+    //switches the side of the card
     public void flip() {
         isLightSideActive = !isLightSideActive;
     }
 
+    // Returns if the card is currently on light or dark side
     public boolean getActiveSide() {
         return isLightSideActive;
     }
 
+    // Print out card's representation depending on which side is active
     public void printCard(){
         if(isLightSideActive){
             System.out.println(this.lightColor + "  " + this.lightType);
